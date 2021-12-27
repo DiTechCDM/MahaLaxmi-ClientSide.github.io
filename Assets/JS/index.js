e50353d84672fafd2d881ab20b0cac001119e372
DiTechCDM <85459137+DiTechCDM@users.noreply.github.com>
2021-12-27 12:41:03 +0530

    index.js

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -65,23 +65,23 @@ function changeToggle(x) {
 
 
 // nav logo shrink script starts here
-window.onscroll = function () {
-    growShrinkLogo()
-};
+// window.onscroll = function () {
+//     growShrinkLogo()
+// };
 
-function growShrinkLogo() {
-    var Logo = document.getElementById("Logo");
-    var top = document.getElementById('setWidth');
-    var top1 = document.getElementById('setwidth2');
-    // var nav = document.getElementById('Navbar');
-    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
-        Logo.style.width = '70px';
-        top.style.marginTop = '1.6rem';
-        top1.style.marginTop = '1.6rem';
-    } else {
-        Logo.style.width = '78px';
-        top.style.marginTop = '1.8rem';
-        top1.style.marginTop = '1.8rem';
-    }
-}
+// function growShrinkLogo() {
+//     var Logo = document.getElementById("Logo");
+//     var top = document.getElementById('setWidth');
+//     var top1 = document.getElementById('setwidth2');
+//     // var nav = document.getElementById('Navbar');
+//     if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+//         Logo.style.width = '70px';
+//         top.style.marginTop = '1.6rem';
+//         top1.style.marginTop = '1.6rem';
+//     } else {
+//         Logo.style.width = '78px';
+//         top.style.marginTop = '1.8rem';
+//         top1.style.marginTop = '1.8rem';
+//     }
+// }
 // nav logo shrink script ends here
